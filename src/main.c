236bc59e936b9ea5ad4b2763cb2a6db817278af6
Grigoriy Efimov <efimov-gm@newpage.xyz>
2023-08-01 23:46:08 +0300

    Add telegram token check

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
+/*
+ *
+ * Copyright (c) 2023 Grigoriy EFimov <efimov-gm@newpage.xyz>
+ * Licensed under the MIT license
+ * 
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]){
+
+
+   const char *TG_TOKEN = getenv("TG_TOKEN");
+   const char *OW_TOKEN = getenv("OW_TOKEN");
+
+   if(!TG_TOKEN){
+       fprintf(stderr, "Telegram token not set. Please fill TG_TOKEN environment variable. Get bot token following instruction: https://core.telegram.org/bots/features#botfather\n");
+       return 1;
+   }
+
 
    printf("Bot is started...\n");
 
