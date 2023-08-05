bc6a30bd17e81499b1c48c34e901148a898dabcb
Grigoriy Efimov <efimov-gm@newpage.xyz>
2023-08-05 11:13:15 +0300

    Split the tg_function and curl_function

---
+++ src/curl_functions.c
@@ -0,0 +1,106 @@
+/*
+ *
+ * Copyright (c) 2023 Grigoriy EFimov <efimov-gm@newpage.xyz>
+ * Licensed under the MIT license
+ * 
+*/
+
+
+#include <curl/curl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+#include "curl_functions.h"
+
+
+size_t noop_cb(void *ptr, size_t size, size_t nmemb, void *data) {
+  return size * nmemb;
+}
+
+size_t char_buff_copy_cb(void *ptr, size_t size, size_t nmemb, mem_buff* mb){
+    
+    size_t length = size*nmemb;
+    char* new;
+
+    new = realloc(mb->buffer,length+1);
+    mb->size = length;
+
+    if(new == NULL){
+        fprintf(stderr, "realloc failed!");
+        exit(1);
+    }
+
+    mb->buffer = new;
+
+    memcpy(mb->buffer, ptr, length);
+
+    mb->buffer[length] = '\0';
+
+    return size*nmemb;
+
+}
+
+char* escape_link(const char* link){
+    // alloc memory for escaped_link, to free use curl_free() insted of free()
+    CURL *curl = curl_easy_init();
+    if(curl){
+
+        char* escaped_link = curl_easy_escape(curl, link, strlen(link));
+        curl_easy_cleanup(curl);
+
+        return escaped_link;
+    }
+    return "\0";
+}
+
+bool simple_get(char* link){
+
+    CURL *curl = curl_easy_init();
+    if(curl){
+
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, link);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, noop_cb);
+
+        res = curl_easy_perform(curl);
+
+        long http_code = 0;
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+
+        curl_easy_cleanup(curl);
+        
+        if(http_code >= 200 && http_code < 300 && res != CURLE_ABORTED_BY_CALLBACK){
+            return true;
+        }
+    } 
+    return false;
+}
+
+bool get_with_responce(const char* link, mem_buff* mb, const unsigned int curl_timeout){
+
+    CURL *curl = curl_easy_init();
+    if(curl){
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, link);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, char_buff_copy_cb);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, mb);
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, curl_timeout);
+
+        res = curl_easy_perform(curl);
+
+        long http_code = 0;
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+        curl_easy_cleanup(curl);
+
+        if(http_code == 200 && res != CURLE_ABORTED_BY_CALLBACK){
+            return true;
+        }
+
+    } 
+
+
+
+}
--- src/tg_functions.c
+++ src/tg_functions.c
@@ -12,71 +12,10 @@
 #include <string.h>
 #include <errno.h>
 
+#include "curl_functions.h"
 #include "tg_functions.h"
 
-size_t noop_cb(void *ptr, size_t size, size_t nmemb, void *data) {
-  return size * nmemb;
-}
 
-size_t char_buff_copy_cb(void *ptr, size_t size, size_t nmemb, mem_buff* mb){
-    
-    size_t length = size*nmemb;
-    char* new;
-
-    new = realloc(mb->buffer,length+1);
-    mb->size = length;
-
-    if(new == NULL){
-        fprintf(stderr, "realloc failed!");
-        exit(1);
-    }
-
-    mb->buffer = new;
-
-    memcpy(mb->buffer, ptr, length);
-
-    mb->buffer[length] = '\0';
-
-    return size*nmemb;
-
-}
-
-char* escape_link(const char* link){
-    // alloc memory for escaped_link, to free use curl_free() insted of free()
-    CURL *curl = curl_easy_init();
-    if(curl){
-
-        char* escaped_link = curl_easy_escape(curl, link, strlen(link));
-        curl_easy_cleanup(curl);
-
-        return escaped_link;
-    }
-    return "\0";
-}
-
-bool simple_get(char* link){
-
-    CURL *curl = curl_easy_init();
-    if(curl){
-
-        CURLcode res;
-        curl_easy_setopt(curl, CURLOPT_URL, link);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, noop_cb);
-
-        res = curl_easy_perform(curl);
-
-
-        long http_code = 0;
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-
-        curl_easy_cleanup(curl);
-        
-        if(http_code >= 200 && http_code < 300 && res != CURLE_ABORTED_BY_CALLBACK){
-            return true;
-        }
-    } 
-    return false;
-}
 
 bool check_tg_token(const char* token){
 
@@ -106,7 +45,7 @@ bool simple_longpoll_get_updates(const char* token, mem_buff* result,const unsig
     return longpoll_get_updates(token, result, 25, 30, 1, offset);
 }
 
-bool longpoll_get_updates(const char* token, mem_buff* result, const unsigned int timeout, const unsigned int curl_timeout, const unsigned int events_limit, const unsigned long offset){
+bool longpoll_get_updates(const char* token, mem_buff* mb, const unsigned int timeout, const unsigned int curl_timeout, const unsigned int events_limit, const unsigned long offset){
     // Before usage, check token with check_tg_token function to prevent buffer overflow
     
     long link_length = 0;
@@ -140,30 +79,8 @@ bool longpoll_get_updates(const char* token, mem_buff* result, const unsigned in
     link_length += 8; // 8 - prev link part length
     strcpy(link_buffer + link_length, int_buffer);
 
-    CURL *curl = curl_easy_init();
-    if(curl){
-        CURLcode res;
-        curl_easy_setopt(curl, CURLOPT_URL, link_buffer);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, char_buff_copy_cb);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, result);
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, curl_timeout);
-
-        res = curl_easy_perform(curl);
-
-        long http_code = 0;
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-        curl_easy_cleanup(curl);
-
-        if(http_code == 200 && res != CURLE_ABORTED_BY_CALLBACK){
-            return true;
-        }
-
-    } 
-
-    errno = 2;
-
     
-    return false;
+    return get_with_responce(link_buffer, mb, curl_timeout);
 
 }
 
