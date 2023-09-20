fe7e71f2e37c96717ce482b1a0ceca3f3130a84d
Grigoriy Efimov <efimov-gm@newpage.xyz>
2023-09-20 23:20:21 +0300

    Add timezone to logger date format

--- src/logger.c
+++ src/logger.c
@@ -15,13 +15,12 @@ FILE *LOGFILE_FD;
 void get_current_datetime(char *buffer) {
   time_t t = time(NULL);
   struct tm tm = *localtime(&t);
-  sprintf(buffer, "%d-%02d-%02d %02d:%02d:%02d", tm.tm_year + 1900,
-          tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+  strftime(buffer, 30, "%F %T %Z", &tm);
 }
 
 void write_log(const char *error_text) {
   if (LOGFILE_FD != NULL) {
-    char datebuffer[20] = ""; // max date text length with \0 = 20
+    char datebuffer[30];
     get_current_datetime(datebuffer);
 
     fprintf(LOGFILE_FD, "[%s]: %s\n", datebuffer, error_text);
